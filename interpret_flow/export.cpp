5d71c69c84a5a8f782f6aa7e34b3c921b3d96c0e
Ned Bingham <edward.bingham@broccolimicro.io>
2025-04-02 11:16:01 -0400

    restructing for new module definition

--- interpret_flow/export.cpp
+++ interpret_flow/export.cpp
@@ -7,23 +7,23 @@
 
 #include <interpret_arithmetic/export_verilog.h>
 
-parse_verilog::assignment_statement export_assign(arithmetic::ConstNetlist nets, int name, arithmetic::expression expr, bool blocking) {
+parse_verilog::assignment_statement export_assign(arithmetic::ConstNetlist nets, clocked::Assign assign) {
 	parse_verilog::assignment_statement result;
 	result.valid = true;
-	result.name = parse_verilog::export_variable_name(name, nets);
-	result.blocking = blocking;
-	result.expr = parse_verilog::export_expression(expr, nets);
+	result.name = parse_verilog::export_variable_name(assign.net, nets);
+	result.blocking = assign.blocking;
+	result.expr = parse_verilog::export_expression(assign.expr, nets);
 	return result;
 }
 
-parse_verilog::continuous export_continuous(arithmetic::ConstNetlist nets, int name, arithmetic::expression expr, bool blocking, bool force) {
+parse_verilog::continuous export_continuous(arithmetic::ConstNetlist nets, clocked::Assign assign, bool force) {
 	parse_verilog::continuous result;
 	result.valid = true;
 	result.force = force;
-	if (expr.is_null()) {
-		result.deassign = parse_verilog::export_variable_name(name, nets);
+	if (assign.expr.is_null()) {
+		result.deassign = parse_verilog::export_variable_name(assign.net, nets);
 	} else {
-		result.assign = export_assign(nets, name, expr, blocking);
+		result.assign = export_assign(nets, assign);
 	}
 	return result;
 }
@@ -59,54 +59,57 @@ parse_verilog::module_def export_module(const clocked::Module &mod) {
 		}
 	}
 
-	for (int i = 0; i < (int)mod.nets.size(); i++) {
-		if (not mod.nets[i].rules.empty() and mod.nets[i].purpose == clocked::Net::WIRE) {
-			if (mod.nets[i].rules.size() > 1u or not mod.nets[i].rules[0].guard.is_valid()) {
-				printf("error: found stateful assignments on wire '%s'\n", mod.nets[i].name.c_str());
-			}
-			result.items.push_back(shared_ptr<parse::syntax>(new parse_verilog::continuous(export_continuous(mod, i, mod.nets[i].rules[0].assign))));
-		}
+	for (auto i = mod.assign.begin(); i != mod.assign.end(); i++) {
+		result.items.push_back(shared_ptr<parse::syntax>(new parse_verilog::continuous(export_continuous(mod, *i))));
 	}
 
-	for (int i = 0; i < (int)mod.nets.size(); i++) {
-		if (not mod.nets[i].rules.empty() and mod.nets[i].purpose == clocked::Net::REG) {
-			parse_verilog::if_statement *cond = new parse_verilog::if_statement();
-			cond->valid = true;
-			cond->condition.push_back(parse_verilog::export_expression(operand(mod.reset, operand::variable), mod));
-			parse_verilog::block_statement reset;
-			reset.valid = true;
-			reset.sub.push_back(shared_ptr<parse::syntax>(new parse_verilog::assignment_statement(export_assign(mod, i, value(0), true))));
-			cond->body.push_back(reset);
+	for (auto i = mod.blocks.begin(); i != mod.blocks.end(); i++) {
+		parse_verilog::if_statement *cond = new parse_verilog::if_statement();
+		cond->valid = true;
+		cond->condition.push_back(parse_verilog::export_expression(operand(mod.reset, operand::variable), mod));
+		parse_verilog::block_statement reset;
+		reset.valid = true;
+		for (auto j = i->reset.begin(); j != i->reset.end(); j++) {
+			reset.sub.push_back(shared_ptr<parse::syntax>(new parse_verilog::assignment_statement(export_assign(mod, *j))));
+		}
+		cond->body.push_back(reset);
 
-			for (int j = 0; j < (int)mod.nets[i].rules.size(); j++) {
-				if (not mod.nets[i].rules[j].guard.is_valid()) {
-					cond->condition.push_back(parse_verilog::export_expression(mod.nets[i].rules[j].guard, mod));
-				}
-
-				parse_verilog::block_statement body;
-				body.valid = true;
-				body.sub.push_back(shared_ptr<parse::syntax>(new parse_verilog::assignment_statement(export_assign(mod, i, mod.nets[i].rules[j].assign, true))));
-				cond->body.push_back(body);
-
-				if (mod.nets[i].rules[j].guard.is_valid()) {
-					if (j != (int)mod.nets[i].rules.size()-1) {
-						printf("warning: ineffective conditions found in stateful assignment for net '%s'\n", mod.nets[i].name.c_str());
-					}
-					break;
-				}
+		bool done = false;
+		for (auto j = i->rules.begin(); j != i->rules.end() and not done; j++) {
+			if (not j->guard.is_valid()) {
+				cond->condition.push_back(parse_verilog::export_expression(j->guard, mod));
 			}
 
-			parse_verilog::trigger *always = new parse_verilog::trigger();
-			always->valid = true;
-			always->condition.valid = true;
-			always->condition.arguments.push_back(parse_verilog::export_expression(operand(mod.clk, operand::variable), mod));
-			always->condition.operations.push_back("posedge");
-			always->body.valid = true;
-			always->body.sub.push_back(shared_ptr<parse::syntax>(cond));
-			result.items.push_back(shared_ptr<parse::syntax>(always));
-		}
-	}
+			parse_verilog::block_statement body;
+			body.valid = true;
+			for (auto k = j->assign.begin(); k != j->assign.end(); k++) {
+				if (mod.nets[k->net].purpose != clocked::Net::REG) {
+					printf("error: found stateful assignments on wire '%s'\n", mod.nets[k->net].name.c_str());
+				}
 
+				body.sub.push_back(shared_ptr<parse::syntax>(new parse_verilog::assignment_statement(export_assign(mod, *k))));
+			}
+			cond->body.push_back(body);
+
+			if (j->guard.is_valid()) {
+				if (std::next(j) != i->rules.end()) {
+					printf("warning: ineffective conditions found in stateful assignment\n");
+				}
+				done = true;
+			}
+		}
+
+		parse_verilog::trigger *always = new parse_verilog::trigger();
+		always->valid = true;
+		always->condition.valid = true;
+		always->condition.level = parse_verilog::expression::get_level("posedge");
+		always->condition.arguments.push_back(parse_verilog::export_expression(i->clk, mod));
+		always->condition.operations.push_back("posedge");
+		always->body.valid = true;
+		always->body.sub.push_back(shared_ptr<parse::syntax>(cond));
+		result.items.push_back(shared_ptr<parse::syntax>(always));	
+	}
+	
 	return result;
 }
 
--- interpret_flow/export.h
+++ interpret_flow/export.h
@@ -6,7 +6,7 @@
 #include <flow/module.h>
 #include <interpret_arithmetic/interface.h>
 
-parse_verilog::assignment_statement export_assign(arithmetic::ConstNetlist nets, int name, arithmetic::expression expr, bool blocking=false);
-parse_verilog::continuous export_continuous(arithmetic::ConstNetlist nets, int name, arithmetic::expression expr=arithmetic::expression(), bool blocking=false, bool force=false);
+parse_verilog::assignment_statement export_assign(arithmetic::ConstNetlist nets, clocked::Assign assign);
+parse_verilog::continuous export_continuous(arithmetic::ConstNetlist nets, clocked::Assign assign, bool force=false);
 parse_verilog::declaration export_declaration(string type, string name, int msb=0, int lsb=0, bool input=false, bool output=false);
 parse_verilog::module_def export_module(const clocked::Module &mod);
